04ee85ea5d06d2839fb964e1309bab520648b706
Daniel Torres <denial.torres@gmail.com>
2021-12-26 13:00:43 -0600

    Video Detail view with embed videos

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,20 @@
 <template>
-  <div v-if="video">
-    {{ video.snippet.title }}
+  <div v-if="video" class="col-md-8">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe
+        class="embed-responsive-item"
+        :src="videoURL"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div class="details">
+      <h4>
+        {{ video.snippet.title }}
+      </h4>
+      <p>
+        {{ video.snippet.description }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -8,7 +22,20 @@
 export default {
   name: "VideoDetail",
   props: ["video"],
+  computed: {
+    videoURL: function () {
+      const videoId = this.video.id.videoId;
+      return `https://www.youtube.com/embed/${videoId}`;
+    },
+  },
 };
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
